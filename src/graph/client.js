66627c677b408fa33683d686506bd1aef0e17574
Seunope <seunope2004@yahoo.co.uk>
2018-07-18 17:28:42 +0100

    npm start

--- src/graph/client.js
+++ src/graph/client.js
@@ -1,8 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://10.0.3.2:4000/graphql",
+  // uri: "http://10.0.3.2:4000/graphql",
+  url: "https://karis-x.herokuapp.com/graphql",
 });
 
 export default client;
-
